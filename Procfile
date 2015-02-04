d14db95fe69865560ecefe211dc49e1f9d4e6c70
Marco <mhernape@utel.edu.mx>
2015-02-04 17:54:22 -0600

    Ajustes

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn quiz_video.wsgi
+
---
+++ quiz/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^vzaar/$', 'quiz.views.vzaar', name='vzaar'),
+)
+
+urlpatterns += staticfiles_urlpatterns()
+
