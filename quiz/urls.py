e2ba93b63e972dc29fe63f132943804c054b3f3c
victor <mini.guero@hotmail.com>
2015-02-04 23:11:15 -0600

    Listos (?) banners

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = patterns('',
     # Examples:
     url(r'^vzaar/$', 'quiz.views.vzaar', name='vzaar'),
+    url(r'^vzaar2/$', 'quiz.views.vzaar2', name='vzaar2'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- quiz/views.py
+++ quiz/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def vzaar(request):
 	return render(request, 'vzaar.html', {})
+
+def vzaar2(request):
+	return render(request, 'vzaar2.html', {})
